c7e72d5345c329e89db643315f6d89b91c1ebff7
JustFakey <xxpsycho1234xx@gmail.com>
2025-08-11 12:50:55 +0200

    Mit Email

--- 02_tables/02_tables.sql
+++ 02_tables/02_tables.sql
@@ -19,8 +19,9 @@ CREATE TABLE IF NOT EXISTS kunde
 (   
     -- wir geben eine ID an und zählen diese hoch im ersten schritt
     k_id INT AUTO_INCREMENT PRIMARY KEY,
-    k_name VARCHAR(45),
-    k_age TINYINT
+    k_name VARCHAR(100),
+    k_age TINYINT,
+    k_mail VARCHAR(100) UNIQUE
 );
 
 SHOW TABLES;
@@ -31,10 +32,10 @@ DESCRIBE kunde;
 
 -- Datensätze / Zeilen
 
-INSERT INTO kunde(k_name,k_age) VALUES("Meier", 28);
-INSERT INTO kunde(k_name,k_age) VALUES("Leizner", 61);
-INSERT INTO kunde(k_name,k_age) VALUES("LI", 26);
-INSERT INTO kunde(k_name,k_age) VALUES("LI", 40);
+INSERT INTO kunde(k_name,k_age,k_mail) VALUES("Meier", 28, "meier@web.de");
+INSERT INTO kunde(k_name,k_age,k_mail) VALUES("Leizner", 61, "Leizner@web.de");
+INSERT INTO kunde(k_name,k_age,k_mail) VALUES("LI", 26, "Li@web.de");
+INSERT INTO kunde(k_name,k_age,k_mail) VALUES("LI", 40,"Li2@web.de");
 -- Tabellendaten anzeigen lassen
 
 SELECT * FROM kunde;
