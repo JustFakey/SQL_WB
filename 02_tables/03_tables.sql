08c958499eeb5cf9a5585307caf7028cde587c51
JustFakey <xxpsycho1234xx@gmail.com>
2025-08-12 11:11:05 +0200

    PW-Abfrage mit SHA2

--- 02_tables/03_tables.sql
+++ 02_tables/03_tables.sql
@@ -21,7 +21,8 @@ CREATE TABLE IF NOT EXISTS kunde
     k_id INT AUTO_INCREMENT PRIMARY KEY,
     k_name VARCHAR(100) NOT NULL DEFAULT "TBA",
     k_age TINYINT NOT NULL DEFAULT "-1",
-    k_mail VARCHAR(100) UNIQUE DEFAULT "TBA"
+    k_mail VARCHAR(100) UNIQUE DEFAULT "TBA",
+    k_pw VARCHAR(100) NOT NULL DEFAULT "TBA"
 );
 
 
@@ -35,10 +36,10 @@ DESCRIBE kunde;
 
 -- Datensätze / Zeilen füllen
 
-INSERT INTO kunde(k_name,k_age,k_mail) VALUES("Meier", 28, "meier@web.de");
-INSERT INTO kunde(k_name,k_age,k_mail) VALUES("Leizner", 61, "Leizner@web.de");
-INSERT INTO kunde(k_name,k_age,k_mail) VALUES("LI", 26, "Li@web.de");
-INSERT INTO kunde(k_name,k_age,k_mail) VALUES("LI", 40,"Li2@web.de");
+INSERT INTO kunde(k_name,k_age,k_mail,k_pw) VALUES("Meier", 28, "meier@web.de",SHA2('meiers123',256));
+INSERT INTO kunde(k_name,k_age,k_mail,k_pw) VALUES("Leizner", 61, "Leizner@web.de",SHA2("leizi354",256));
+INSERT INTO kunde(k_name,k_age,k_mail,k_pw) VALUES("LI", 26, "Li@web.de",SHA2("lilili",256));
+INSERT INTO kunde(k_name,k_age,k_mail,k_pw) VALUES("LI", 40,"Li2@web.de",SHA2("lilalunurdermannimmondschautzu",256));
 INSERT INTO kunde() VALUES();
 
 
