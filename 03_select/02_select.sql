dd32aa2a93e0c2127fd890aae40e82459efbfcb5
JustFakey <xxpsycho1234xx@gmail.com>
2025-08-12 12:49:37 +0200

    PUSH PUSH PUSH

--- 03_select/02_select.sql
+++ 03_select/02_select.sql
@@ -1,3 +1,18 @@
 -- Hauptthema "SELECT" _02.Select
 -- Select Befehle
 
+-- Thema 1: Basic select
+
+-- Alles Abfragen:
+/*SELECT 
+*
+FROM 
+
+test.contacts;*/
+
+-- Nur eine gewisse spalte abfragen:
+SELECT
+k_id,  #jegliche Spalte, die gegeben ist, kann hier abgefragt werden
+k_email
+FROM
+test.contacts;
