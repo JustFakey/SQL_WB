0ab78535d050df46365382e50f2e1d64e57ea231
JustFakey <xxpsycho1234xx@gmail.com>
2025-08-13 12:11:03 +0200

    Update

--- 03_select/02_select.sql
+++ 03_select/02_select.sql
@@ -11,8 +11,22 @@ FROM
 test.contacts;*/
 
 -- Nur eine gewisse spalte abfragen:
+-- SELECT
+-- k_id,  #jegliche Spalte, die gegeben ist, kann hier abgefragt werden
+-- k_email,
+-- k_city
+-- FROM test.contacts
+-- WHERE city = 'Berlin' #Hier wird gesagt, dass nur Kunden die Berlin angegeben haben als Stadt
+-- WHERE city LIKE 'Fra%' #sucht nach Kunden, die in einer Stadt wohnen die mit "Fra" anfängt "%" ist hier ein platzhalter
+-- ORDER BY name ASC (wird aufsteigend nach namen sortiert)
+-- ORDER BY name DESC (wird absteigend nach namen sortiert)
+-- AS = einen Alias für den genannten text erstellen (Ändert die Überschriften)
+
 SELECT
-k_id,  #jegliche Spalte, die gegeben ist, kann hier abgefragt werden
-k_email
-FROM
-test.contacts;
+name AS Name,
+email,
+city
+FROM languages.contacts
+WHERE city LIKE '%lin'
+ORDER BY name DESC
+;
